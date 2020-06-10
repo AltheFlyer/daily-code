e9e0f84b76bee1d2b60d6c7d65ea5a0116e0baa6
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2020-06-10 02:56:14 -0400

    better graphics but still broken :/

--- radial-menu/radial.js
+++ radial-menu/radial.js
@@ -1,4 +1,6 @@
 let radialMenus = document.querySelectorAll(".radial-menu");
+let children = []
+
 
 start();
 
@@ -12,14 +14,13 @@ function start() {
     radialMenus.forEach(function(radialMenu) {
         let c = radialMenu.querySelectorAll(".radial-child");
         //let children = radialMenu.children;
-        let children = [];
+        children = [];
         c.forEach(function(child) {
             if (child.parentElement === radialMenu) {
                 children.push(child);
             }
         });
 
-        console.log(children);
         let deltaAngle = (Math.PI * 2) / children.length;
 
         let angle = 0;
@@ -32,11 +33,14 @@ function start() {
             let c = Math.cos(angle);
             let s = Math.sin(angle);
             child.style.transitionDelay = time + "s";
-            child.style.transform = generateTranslation(c * (gap + mainHeight / 2 + childHeight / 2), s * (gap + mainHeight / 2 + childHeight / 2));        
+            child.style.left = c * (gap + mainHeight / 2 + childHeight / 2) + childHeight / 2 + "vmin";
+            child.style.top = s * (gap + mainHeight / 2 + childHeight / 2) + childHeight / 2 + "vmin";
             child.style.visibility = "visible";
             angle += deltaAngle;
             time += 0.1;
-            child.addEventListener("click", promote);
+            if (child.querySelectorAll(".radial-child").length > 0) {
+                child.addEventListener("click", promote);
+            }
         });
     });
 }
@@ -45,26 +49,34 @@ function promote(event) {
     let child = event.currentTarget;
     let main = child.parentElement;
 
+    child.style.zIndex = 0;
     child.style.transitionDelay = "0s";
-    child.style.transform = "translate(0, 0)";
     child.style.left = 0;
     child.style.top = 0;
     child.style.height = "40vmin";
     child.style.width = "40vmin";
+    child.style.backgroundColor = "#cccccc";
+    child.style.fontSize = "100%";
 
+    children.forEach(function(c) {
+        if (c != child) {
+            console.log(c);
+            c.style.transitionDelay = "0s";
+            c.style.left = "10vmin";
+            c.style.top = "10vmin";
+        }
+    })
 
-    console.log("A");
     setTimeout(function() {
         child.removeEventListener("click", promote);
         child.className = "radial-menu";
-        main.className = "";
-        child.style.top = "";
-        child.style.left = "";
-        child.style.margin = "auto";
-        console.log("K");
+        child.classList.add("circle");
+        child.style.zIndex = 10;
+        child.style = "";
+        main.className="";
+        //main.className = "hidden-radial-menu";
         start();
-    }, 1000);
-    console.log("B");
+    }, 700);
 }
 
 function generateTranslation(x, y) {
